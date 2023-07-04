3c5eb4293c3cc4610228ab32f855b2e3b7d7d8be
soon91 <vbc1124@gmail.com>
2023-07-05 00:19:00 +0900

    로그인 API 구현 (with JWT)

--- src/main/java/com/sparta/blog/controller/UserController.java
+++ src/main/java/com/sparta/blog/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sparta.blog.controller;
 
+import com.sparta.blog.dto.LoginRequestDto;
 import com.sparta.blog.dto.SignupRequestDto;
 import com.sparta.blog.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,4 +23,10 @@ public class UserController {
         return "회원가입 성공";
     }
 
+    @PostMapping("/user/login")
+    public String login(@RequestBody LoginRequestDto requestDto, HttpServletResponse res) {
+        userService.login(requestDto, res);
+
+        return "로그인 성공";
+    }
 }
--- src/main/java/com/sparta/blog/service/UserService.java
+++ src/main/java/com/sparta/blog/service/UserService.java
@@ -1,8 +1,11 @@
 package com.sparta.blog.service;
 
+import com.sparta.blog.dto.LoginRequestDto;
 import com.sparta.blog.dto.SignupRequestDto;
 import com.sparta.blog.entity.User;
+import com.sparta.blog.jwt.JwtUtil;
 import com.sparta.blog.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +16,13 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     public void signup(SignupRequestDto requestDto) {
@@ -35,4 +40,22 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        // 사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        // 비밀번호 확인
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+        String token = jwtUtil.createToken(user.getUsername());
+        jwtUtil.addJwtToCookie(token, res);
+    }
 }
